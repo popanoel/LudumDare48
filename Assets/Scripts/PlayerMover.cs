be0e6e408e51da3817a005b58b483ef48326ba44
Vinc <flutbon@gmail.com>
2021-04-24 14:58:24 -0400

    v0.0.2 - I'm late AF

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour{
+   //Movement variables
+    private float _H=0f;
+    private float _V=0f;
+    private Rigidbody2D _monBody;
+    // private AudioSource _monSon;
+
+    [SerializeField][Range(1f,6f)]
+    private float _Speed=0f;
+
+    //State bool
+    private bool _isBusy=false;
+    void Awake(){
+        _monBody=GetComponent<Rigidbody2D>();
+    //    _monSon=GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //_isBusy=GameManager.Instance._IsTalking;
+        if(_isBusy){
+            _H=0f;
+            _V=0f;
+            return;
+        }
+
+        _H=Input.GetAxis("Horizontal");
+        _V=Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate(){
+        _monBody.velocity=new Vector3(_H*_Speed,_V*_Speed,0f);
+        
+    }
+
+
+    public Vector3 GetMcPos {
+        get{
+            return transform.position;
+        }
+    }
+}
